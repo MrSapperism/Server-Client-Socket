8a1fd1845910e9248f2799ab20b333036240d49b
Christopher Michael Sutton <christopher.sutton12@gmail.com>
2020-04-06 15:07:35 +1000

    working with fixed server size

--- Client.java
+++ Client.java
@@ -10,21 +10,20 @@ public class Client {
         try {
             clientSocket = new Socket(ip, port);
             out = new DataOutputStream(clientSocket.getOutputStream());
-            in = new BufferedReader(
-                     new InputStreamReader(clientSocket.getInputStream()));
-        } catch(final Exception e){
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (final Exception e) {
             System.out.println(e);
         }
-     }
+    }
 
     public String sendMessage(final String msg) {
-        char resp[] = new char[200];
-        try{
+        final char resp[] = new char[200];
+        try {
             out.write(msg.getBytes());
             out.flush();
             in.read(resp);
             return new String(resp);
-        } catch(final Exception e){
+        } catch (final Exception e) {
             System.out.println(e);
         }
         return new String(resp);
@@ -35,10 +34,17 @@ public class Client {
             in.close();
             out.close();
             clientSocket.close();
-        } catch(final Exception e){
+        } catch (final Exception e) {
             System.out.println(e);
         } finally {
         }
+    }
+
+    public void shcedule_job(String status){
+        final String[] res = status.split(" ");
+        status =this.sendMessage("SCHD "+ res[2]+" large 0 ");
+        System.out.println(status);
+
     }
 
     public static void main(final String args[]){
@@ -52,26 +58,25 @@ public class Client {
         client.startConnection("127.0.0.1", 50000);
 
         String in_msg = "";
-        int job_id = 0;
+        final int job_id = 0;
         in_msg = client.sendMessage("HELO");
         System.out.println(in_msg);
         in_msg = client.sendMessage("AUTH comp355");
         System.out.println(in_msg);
         in_msg = client.sendMessage("REDY");
-        System.out.println(in_msg);
-        if(in_msg.contains("JOBN")){
-            String[] res = in_msg.split(" ");
-            in_msg = client.sendMessage("RESC ALL" +
-                                        " " + res[4] +
-                                        " " + res[5] +
-                                        " " + res[6]);
-            System.out.println(in_msg);
-            in_msg = client.sendMessage("SCHD 0 large 0 ");
-            System.out.println(in_msg);
-        } else {
-            in_msg = client.sendMessage("QUIT");
+        while(in_msg.contains("JOBN")){
+            client.shcedule_job(in_msg);
+            in_msg = client.sendMessage("REDY");
         }
+
         System.out.println(in_msg);
+        client.sendMessage("QUIT");
     }
 
 }
+
+/*in_msg = client.sendMessage("RESC ALL" +
+  " " + res[4] +
+  " " + res[5] +
+  " " + res[6]);
+*/
