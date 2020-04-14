a7d7d7d4746c250a40760272b1f3833b6a48d89e
Christopher Michael Sutton <christopher.sutton12@gmail.com>
2020-04-14 23:49:13 +1000

    added parser for proj

---
+++ Parser.java
@@ -0,0 +1,76 @@
+import javax.xml.parsers.*;
+import java.io.*;
+import org.w3c.dom.*;
+import java.util.*;
+
+class Parser {
+
+	File input_file;
+	DocumentBuilderFactory dbFactory;
+	DocumentBuilder dBuilder;
+	Document doc;
+	Element root;
+
+	public Parser(){
+		try {
+			this.dbFactory = DocumentBuilderFactory.newInstance();
+			this.dBuilder = this.dbFactory.newDocumentBuilder();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public Parser(String input_file){
+		try {
+			this.input_file = new File(input_file);
+			this.dbFactory = DocumentBuilderFactory.newInstance();
+			this.dBuilder = this.dbFactory.newDocumentBuilder();
+			this.doc = this.dBuilder.parse(this.input_file);
+			this.doc.getDocumentElement().normalize();
+			this.root = this.doc.getDocumentElement();
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+	public static void PrintElement(Element elmnt, String name) {
+		System.out.println(elmnt.getNodeName());
+		NodeList nodes = elmnt.getElementsByTagName(name);
+		PrintAttribute(nodes);
+	}
+
+	public static void PrintAttribute(NodeList nodes) {
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node child = nodes.item(i);
+			System.out.print("Node: " + child.getNodeName());
+			if (child.getNodeType() == Node.ELEMENT_NODE) {
+				NamedNodeMap attribs = child.getAttributes();
+				for (int y = 0; y < attribs.getLength(); y++) {
+					Node temp = attribs.item(y);
+					System.out.print(" " + temp.getNodeName() + "=" + temp.getNodeValue());
+				}
+				System.out.println("");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			Parser parser = new Parser("./ds-sim/system.xml");
+
+			Element server_root = (Element) parser.root.getElementsByTagName("servers").item(0);
+			//Element job_root = (Element) root.getElementsByTagName("jobs").item(0);
+			//NodeList workload = root.getElementsByTagName("workload");
+			//Element termination_root = (Element) root.getElementsByTagName("termination").item(0);
+
+			PrintElement(server_root, "server");
+			//PrintElement(job_root, "job");
+			//PrintAttribute(workload);
+			//PrintElement(termination_root, "condition");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
